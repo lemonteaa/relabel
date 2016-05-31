20da7714b52df88e39f19e0d2799e90d425a7696
lemonteaa <lemontea.Tom@gmail.com>
2016-05-31 23:39:44 +0800

    Add another test for converter

--- test/relabel/lazy_test.clj
+++ test/relabel/lazy_test.clj
@@ -10,8 +10,19 @@
   (is (thrown? AssertionError ((from :baz) {:bar 1 :foo "c"}))))
 
 (deftest converter-main
-  (let [in { :username "nick" :age 34 :college "abc" }
-        res { :name "nick" :age 34 :school "abc" }]
-  (is (= res ((converter { :name (from :username)
-                           :age (from :age)
-                           :school (from :college) }) in)))))
+  (let [in1 { :username "nick" :age 34 :college "abc" }
+        res1 { :name "nick" :age 34 :school "abc" }
+        in2 { :username "mary"
+              :submissions [{ :title "abc" :keyword "lab" :submission-date nil }
+                            { :title "def" :keyword "Dunning-Kruger effect" :submission-date "44" }] }
+        res2 { :name "mary"
+               :posts [{ :title "abc" :hashtag "lab" :date nil }
+                       { :title "def" :hashtag "Dunning-Kruger effect" :date "44" }] }]
+    (is (= res1 ((converter { :name (from :username)
+                              :age (from :age)
+                              :school (from :college) }) in1)))
+    (is (= res2 ((converter { :name (from :username)
+                              :posts (comp (partial map (converter { :title (from :title)
+                                                                     :hashtag (from :keyword)
+                                                                     :date (from :submission-date) }))
+                                           (from :submissions)) }) in2)))))
